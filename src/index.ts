ca91e0863d672c16b6f68555de56f4bd246a53fb
sidsitaula <siddharthasitaula02@gmail.com>
2022-08-12 19:51:33 +0545

    initial source file

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,28 @@
-type geda ={
-    name:string,
-    address:string
-}
+// @errors: 2578
+type JSONObject = any
+type JSONArray = any
+type JSONValue = any
 
-interface peda {
-    name:string
-    address:string
-}
-console.log("Syahahahh")
+////// DO NOT EDIT ANY CODE BELOW THIS LINE //////
+function isJSON(arg: JSONValue) {}
+
+// POSITIVE test cases (must pass)
+isJSON("hello")
+isJSON([4, 8, 15, 16, 23, 42])
+isJSON({ greeting: "hello" })
+isJSON(false)
+isJSON(true)
+isJSON(null)
+isJSON({ a: { b: [2, 3, "foo"] } })
+
+// NEGATIVE test cases (must fail)
+// @ts-expect-error
+isJSON(() => "")
+// @ts-expect-error
+isJSON(class {})
+// @ts-expect-error
+isJSON(undefined)
+// @ts-expect-error
+isJSON(new BigInt(143))
+// @ts-expect-error
+isJSON(isJSON)
